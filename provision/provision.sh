ab7abbc379d9a009e85425fd237773771b41ae5a
jose pardo <oldlastman@gmail.com>
2013-06-26 22:05:27 +0200

    eliminado wp unit test

--- provision/provision.sh
+++ provision/provision.sh
@@ -313,21 +313,7 @@ else
 	git pull --rebase origin development
 fi
 
-# Checkout and configure the WordPress unit tests
-if [ ! -f /home/vagrant/flags/disable_wp_tests ]
-then
-	if [ ! -d /srv/www/wordpress-unit-tests ]
-	then
-		printf "Downloading WordPress Unit Tests.....https://unit-tests.svn.wordpress.org\n"
-		# Must be in a WP directory to run wp
-		cd /srv/www/wordpress-trunk
-		wp core init-tests /srv/www/wordpress-unit-tests --dbname=wordpress_unit_tests --dbuser=wp --dbpass=wp
-	else
-		printf "Updating WordPress unit tests...\n"	
-		cd /srv/www/wordpress-unit-tests
-		svn up --ignore-externals
-	fi
-fi
+
 
 # Download phpMyAdmin 4.0.3
 if [ ! -d /srv/www/default/database-admin ]
